c8b2bcad63f0b9c9b78df1a31c1ba7ba03e85e84
Edward Kemp <edwardlanekemp@gmail.com>
2016-07-11 10:53:23 +0100

    added styling and a loosing condition

--- scripts/oop.js
+++ scripts/oop.js
@@ -5,8 +5,13 @@ function Player(element) {
   this.element = element;
   this.lives = 3;
   this.score = 0;
+  this.highScore = 0;
 
 }
+
+var myAudio = new Audio("music/div.mp3");
+
+
 var player = new Player(document.querySelector('.box'));
 
 game.randomDiv = function(random) {
@@ -18,8 +23,6 @@ game.randomDiv = function(random) {
   }
 
 }
-
-
 game.randomDiv();
 
 game.placeRanomDivs = function(randomDiv) {
@@ -30,93 +33,126 @@ game.placeRanomDivs = function(randomDiv) {
     this.top = Math.floor(Math.random() * parseInt(this.container.offsetTop)) + 'px';
     this.width = Math.floor(Math.random() * 800) + 'px';
     this.height = Math.floor(Math.random() * 800) + 'px';
-    // this.allSides = this.generatedDivs[i].getBoundingClientRect();
 
     this.generatedDivs[i].style.left = this.left;
     this.generatedDivs[i].style.top = this.top++;
 
     this.generatedDivs[i].style.width = this.width;
     this.generatedDivs[i].style.height = this.height;
-    // console.log("left position is ", this.generatedDivs[i].style.left);
-    console.log('height is ' + this.top);
 
-    // game.collisionDetection(this.top, this.left, this.width, this.height);
   }
 
 }
+
+game.reset = function(){
+  this.gamOverButotn = document.querySelector('.')
+}
+
 game.setTransition = function(lotsOfBlocks) {
-  this.lotsOfBlocks = document.querySelectorAll('.random');
-  this.score = document.getElementById('actual-score');
-  var self = this;
-  this.goUp = 0;
-  setInterval(function() {
-    self.position = player.element.getBoundingClientRect();
+    this.lotsOfBlocks = document.querySelectorAll('.random');
+    this.score = document.getElementById('actual-score');
+    this.highScore = document.getElementById('high-score');
+    this.lives = document.querySelector('.lives');
+    this.startOver = document.querySelector('.gameover-bg');
+    var self = this;
+    this.goUp = 0;
+    this.gameAnimations = setInterval(function() {
+        self.position = player.element.getBoundingClientRect();
+        self.position.top = player.element.top;
+        self.position.left = player.element.left;
+        self.position.width = player.element.height;
+        self.position.height = player.element.style.width;
 
-    self.position.top = player.element.top;
-    self.position.left = player.element.left;
-    self.position.width = player.element.height;
-    self.position.height = player.element.style.width;
+        self.total = self.position.top + self.position.height;
+        self.total2 = self.position.left + self.position.width;
 
-    self.total = self.position.top + self.position.height;
-    self.total2 =  self.position.left + self.position.width;
-    console.log(self.totals, self.total2);
+        for (var i = 0; i < 100; i++) {
 
-    console.log(self.position.left);
-    for (var i = 0; i < 100; i++) {
-      self.lotsOfBlocks[i].style.top = self.goUp-- + 'px';
+          self.lotsOfBlocks[i].style.top = self.goUp-- + 'px';
 
-      //Get Position of all sides
-      self.computerPosition = self.lotsOfBlocks[i].getBoundingClientRect();
+          //Get Position of all sides
+          self.computerPosition = self.lotsOfBlocks[i].getBoundingClientRect();
 
-      self.computerPosition.top = self.lotsOfBlocks[i].top;
-      self.computerPosition.left = self.lotsOfBlocks[i].left;
-      self.computerPosition.width = self.lotsOfBlocks[i].height;
-      self.computerPosition.height = self.lotsOfBlocks[i].style.width;
+          self.computerPosition.top = self.lotsOfBlocks[i].top;
+          self.computerPosition.left = self.lotsOfBlocks[i].left;
+          self.computerPosition.width = self.lotsOfBlocks[i].height;
+          self.computerPosition.height = self.lotsOfBlocks[i].style.width;
 
-      self.total3 = self.computerPosition.top + self.computerPosition.height;
-      self.total4 =  self.computerPosition.left + self.computerPosition.width;
+          self.total3 = self.computerPosition.top + self.computerPosition.height;
+          self.total4 = self.computerPosition.left + self.computerPosition.width;
 
-       console.log(self.total3);
 
-      if ( self.total < self.computerPosition.top || self.position.top > self.total3 ||
-        self.total2 <   self.computerPosition.left || self.position.left > self.total4  ){
-         console.log('your okay')
-        }else{
-          alert('your hit!!')
-        }
+          if (self.total < self.computerPosition.top || self.position.top > self.total3 ||
+            self.total2 < self.computerPosition.left || self.position.left > self.total4) {
 
-      self.score.innerHTML = 'Score : ' + parseInt(player.score += 1 / 50);
+          } else {
+
+            self.lives.innerHTML = 'Lives: ' + player.lives--;
+            self.score.innerHTML = 'Score : ' + parseInt(player.score--);
+
+
+              if (player.lives < 0) {
+                console.log('gameover');
+                clearInterval(self.gameAnimations);
+                myAudio.pause();
+                self.highScore.innerHTML = 'High-Score: ' + parseInt(player.score++);
+                self.score.innerHTML = 'Score : ' + 0;
+                self.startOver.style.display = 'inherit';
+                self.lotsOfBlocks[i].style.top = self.placeRanomDivs.top;
+              }
+
+            }
+            self.score.innerHTML = 'Score : ' + parseInt(player.score+=1 / 60);
+          }
+        }, 150);
     }
-  }, 1000);
-}
 
 
-game.setTransition()
 
-game.placeRanomDivs();
+    game.gameNotifications = function() {
+      this.tutBoxButton = document.querySelector('.button');
+      this.tutBoxBg = document.querySelector('.tutorial-bg');
+      this.tutBox = document.querySelector('.tutorial');
+      var self = this;
+      this.tutBoxButton.addEventListener('click', function() {
+        this.tutBoxBg.style.display = 'none';
+        console.log(this);
 
 
-Player.prototype.move = function(key) {
-  switch (key) {
-    case 'ArrowLeft':
-      this.element.style.left = this.element.offsetLeft - 100 + 'px'
+        console.log('click!')
+      }.bind(this));
 
-      break;
-    case 'ArrowRight':
-      this.element.style.left = this.element.offsetLeft + 100 + 'px'
+    }
 
-      break;
-      //   case 'ArrowUp':
-      //     this.element.style.top = this.element.offsetTop - 100 + 'px'
-      //
-      //     break;
-      //   case 'ArrowDown':
-      //     this.element.style.top = this.element.offsetTop + 100 + 'px'
-      //     break;
-      //
-  }
-}
+    game.gameNotifications();
 
-document.onkeydown = function() {
-  console.log(player.move(event.key));
-}
+
+    Player.prototype.move = function(key) {
+      switch (key) {
+        case 'ArrowLeft':
+          this.element.style.left = this.element.offsetLeft - 150 + 'px';
+          break;
+        case 'ArrowRight':
+          this.element.style.left = this.element.offsetLeft + 150 + 'px';
+          break;
+        case 'Enter':
+          game.setTransition();
+          game.placeRanomDivs();
+          myAudio.play();
+          myAudio.volume = 0.1;
+          break;
+
+          //   case 'ArrowUp':
+          //     this.element.style.top = this.element.offsetTop - 100 + 'px'
+          //
+          //     break;
+          //   case 'ArrowDown':
+          //     this.element.style.top = this.element.offsetTop + 100 + 'px'
+          //     break;
+          //
+      }
+    }
+
+    document.onkeydown = function(event) {
+      console.log(player.move(event.key));
+    }
