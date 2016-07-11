9baada0e241dab784132725138dd151160bff014
Edward Kemp <edwardlanekemp@gmail.com>
2016-07-11 12:31:21 +0100

    changed server

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('Hello World\n');
-}).listen(process.argv[2]);
-console.log('Server running at http://127.0.0.1:' + process.argv[2]);
+}).listen(process.env.PORT || 5000)
+console.log('Server running at http://127.0.0.1:' + process.env.PORT);
